4edcc7f811aa9a350033d80d1b2968642b786783
Amarnath <amarnath.sundaram@hotmail.fr>
2019-06-17 13:13:56 +0200

    premier composant

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Bloc from "./components/Bloc";
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Bloc />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Bloc.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Bloc extends React.Component {
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
+
+export default Bloc;
