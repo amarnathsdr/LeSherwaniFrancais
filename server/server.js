28995098218bf486bb50e9dee1f4c496c722a5c8
Amarnath <amarnath.sundaram@hotmail.fr>
2019-09-30 13:22:56 +0200

    set correct data to deploy on heroku

--- server/server.js
+++ server/server.js
@@ -44,9 +44,9 @@ mongoose
   });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname + "../client/build/index.html"));
   });
 }
 
