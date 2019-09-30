9cdd5c3c563550533fc8ddc4483e314be293dfd4
Amarnath <amarnath.sundaram@hotmail.fr>
2019-09-30 14:59:45 +0200

    path and repository

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const mongoose = require("mongoose");
