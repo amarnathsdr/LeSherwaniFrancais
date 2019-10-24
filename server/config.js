ca0527f29a809d5e9bced81c592b1b10bf33907b
Amarnath <amarnath.sundaram@hotmail.fr>
2019-10-24 18:10:30 +0200

    useless code

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ const uri = `mongodb+srv://${process.env.MONGO_USER}:${
   process.env.MONGO_PASSWORD
 }@cluster0-rsdbl.mongodb.net/${
   process.env.MONGO_DB
-}?authSource=admin&retryWrites=true&w=1&authMechanism=SCRAM-SHA-1`;
+}?retryWrites=true&w=majority`;
 
 mongoose
   .connect(
